f18d27360b96efecc9cdade770dfddfe1c4961e3
Kim ji hoon <Nwb10102@gmail.com>
2025-10-28 00:18:57 +0900

    폰트 추가 및 게임 매니저 몇몇개 추가

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,10 +1,17 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance; // 싱글톤 인스턴스
+    public static GameManager Instance;
 
     public int playerCoins = 0;
+    public int failProtection = 0;
+
+    [Space]
+    [Header("Text UI Elements")]
+    public TextMeshProUGUI coinText;
+    public TextMeshProUGUI failProtectionText;
 
     void Awake()
     {
@@ -14,14 +21,39 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
-        
+
         Instance = this;
-        DontDestroyOnLoad(gameObject); // 씬 이동시 유지
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void UseFailProtection()
+    {
+        if (failProtection > 0)
+        {
+            failProtection--;
+            Debug.Log("실패 보호 사용! 남은 보호 횟수: " + failProtection);
+        }
+        else
+        {
+            Debug.Log("실패 보호가 없습니다!");
+        }
+    }
+    
+    public void AddFailProtection(int amount)
+    {
+        failProtection += amount;
+        Debug.Log("실패 보호 추가! 현재 보호 횟수: " + failProtection);
     }
 
     public void AddCoins(int amount)
     {
         playerCoins += amount;
         Debug.Log("현재 코인: " + playerCoins);
+    }
+
+    void Update()
+    {
+        coinText.text = "코인 : " + playerCoins;
+        failProtectionText.text = "실패방지서 : " + failProtection;
     }
 }
